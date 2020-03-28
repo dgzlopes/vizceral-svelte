ec266b490875e40a8809c1df2afe6aace68d8e6e
Daniel González Lopes <danielgonzalezlopes@gmail.com>
2020-03-28 12:19:57 +0100

    Add filters and modes
    Signed-off-by: Daniel González Lopes <danielgonzalezlopes@gmail.com>

--- src/Vizceral.svelte
+++ src/Vizceral.svelte
@@ -1,32 +1,44 @@
 <script>
   import Vizceral from "vizceral";
-  import { onMount } from 'svelte';
+  import { onMount } from "svelte";
   let viz;
   let running = false;
   export let traffic;
-  export let styles;
+  export let styles = {};
+  export let filters = [];
+  export let modes;
 
   onMount(async () => {
-		setTimeout(function() {
+    setTimeout(function() {
       run();
     }, 100);
-	});
+  });
 
   function run() {
     running = true;
     viz = new Vizceral(document.getElementById("vizceral"));
     viz.updateStyles(styles);
     viz.updateData(traffic);
+    viz.setFilters(filters);
+    viz.setModes(modes);
     viz.setView();
     viz.animate();
   }
 
-  $: if(traffic && running){
+  $: if (traffic && running) {
     viz.updateData(traffic);
   }
 
-  $: if(styles && running){
+  $: if (styles && running) {
     viz.updateStyles(styles);
+  }
+
+  $: if (filters && running) {
+    viz.setFilters(filters);
+  }
+
+  $: if (modes && running) {
+    viz.setModes(modes);
   }
 </script>
 
